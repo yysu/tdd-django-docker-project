a4baeb670e96a7607bd70b83c186d9ead33bd1bd
YYSU <jim123820@gmail.com>
2022-11-25 20:14:46 +0800

    add pytest

---
+++ app/tests/test_foo.py
@@ -0,0 +1,20 @@
+import json
+
+from django.urls import reverse
+
+def test_hello_world():
+    assert "hello_world" == "hello_world"
+    assert "foo" != "bar"
+
+def test_ping(client):
+    # Given
+    # client
+
+    # When
+    url = reverse("ping")
+    response = client.get(url)
+    content = json.loads(response.content)
+
+    # Then
+    assert response.status_code == 200
+    assert content["ping"] == "pong!"
